9c364ea2dd2c505bbdac4360ed8fa4fe391562d2
hideyuki-aoki <aoki.ba4@gmail.com>
2020-02-23 14:43:50 +0900

    modify like convert

--- src/main/kotlin/com/ppap/torimocore/presentation/dto/ArticleLikeDto.kt
+++ src/main/kotlin/com/ppap/torimocore/presentation/dto/ArticleLikeDto.kt
@@ -6,9 +6,7 @@ data class ArticleLikeFormDto(
      val articleId: Long,
      val userId: Long
 ) {
-    companion object {
-        fun convert(form: ArticleLikeFormDto): ArticleLike {
-            return ArticleLike(0, form.articleId, form.userId)
-        }
+    fun convert(): ArticleLike {
+        return ArticleLike(0, this.articleId, this.userId)
     }
 }
