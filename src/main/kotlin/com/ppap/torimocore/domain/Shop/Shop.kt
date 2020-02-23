e3f44e49801381496c05d56577c6400e3ee30049
misaosyushi <misaki.tsujita@rakus-partners.co.jp>
2020-02-23 23:19:57 +0900

    formatter

--- src/main/kotlin/com/ppap/torimocore/domain/Shop/Shop.kt
+++ src/main/kotlin/com/ppap/torimocore/domain/Shop/Shop.kt
@@ -5,11 +5,11 @@ import javax.persistence.*
 
 @Entity
 @Table(name = "shops")
-data class Shop (
-    @Id
-    @GeneratedValue
-    val id: Long,
+data class Shop(
+        @Id
+        @GeneratedValue
+        val id: Long,
 
-    @Column(name = "name", nullable = false)
-    val name: String
-): Serializable
+        @Column(name = "name", nullable = false)
+        val name: String
+) : Serializable
