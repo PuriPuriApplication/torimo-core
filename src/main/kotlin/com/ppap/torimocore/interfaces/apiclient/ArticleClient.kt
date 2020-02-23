06a4f5d3420f228ff2e62ec344910a4a7a2b142c
hideyuki-aoki <aoki.ba4@gmail.com>
2020-02-23 20:52:52 +0900

    add method

--- src/main/kotlin/com/ppap/torimocore/interfaces/apiclient/ArticleClient.kt
+++ src/main/kotlin/com/ppap/torimocore/interfaces/apiclient/ArticleClient.kt
@@ -1,5 +1,6 @@
 package com.ppap.torimocore.interfaces.apiclient
 
+import com.ppap.torimocore.domain.Article.Article
 import retrofit2.Call
 import retrofit2.http.POST
 import retrofit2.http.PUT
@@ -12,6 +13,7 @@ interface ArticleClient {
     fun postArticle(article: Article): Call<ArticleDto>
 
     @PUT("/update")
+    fun updateArticle(article: Article): Call<ArticleDto>
 
 }
 
