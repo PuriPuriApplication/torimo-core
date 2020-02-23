ae005b34abcdd86f611d9d699443be2aa1df2be6
misaosyushi <misaki.tsujita@rakus-partners.co.jp>
2020-02-23 17:16:49 +0900

    delete test_teble

--- src/main/kotlin/com/ppap/torimocore/domain/TestTable/TestTableRepository.kt
+++
@@ -1,8 +0,0 @@
-package com.ppap.torimocore.domain.TestTable
-
-import org.springframework.data.jpa.repository.JpaRepository
-import org.springframework.stereotype.Repository
-
-@Repository
-interface TestTableRepository : JpaRepository<TestTable, Long> {
-}
