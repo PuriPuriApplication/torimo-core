7f801124860f9e3a2c18fca0e58863ec902c64bd
hideyuki-aoki <aoki.ba4@gmail.com>
2020-03-01 17:59:39 +0900

    modify error handling

--- src/main/kotlin/com/ppap/torimocore/application/handler/ErrorHandler.kt
+++ src/main/kotlin/com/ppap/torimocore/application/handler/ErrorHandler.kt
@@ -1,14 +1,15 @@
 package com.ppap.torimocore.application.handler
 
 import org.springframework.http.HttpStatus
+import org.springframework.http.ResponseEntity
 import org.springframework.security.authentication.BadCredentialsException
-import java.util.HashMap
 import org.springframework.web.bind.annotation.ResponseBody
 import org.springframework.web.HttpRequestMethodNotSupportedException
 import org.springframework.web.bind.annotation.ControllerAdvice
 import org.springframework.web.bind.annotation.ExceptionHandler
 import org.springframework.web.bind.annotation.ResponseStatus
 
+typealias Response = Map<String, Any?>
 
 @ControllerAdvice
 class ExceptionHandler {
@@ -16,22 +17,17 @@ class ExceptionHandler {
     @ResponseStatus(HttpStatus.METHOD_NOT_ALLOWED)
     @ExceptionHandler(HttpRequestMethodNotSupportedException::class)
     @ResponseBody
-    fun handleError(): Map<String, Any> {
-        val errorMap = HashMap<String, Any>()
-        return mapOf(
-                "statusCode" to HttpStatus.METHOD_NOT_ALLOWED.value().toString(),
-                "message" to "許可されていないメソッドです"
-        )
-    }
+    fun handleError(): ResponseEntity<Response> = ResponseEntity(mapOf(
+            "statusCode" to HttpStatus.METHOD_NOT_ALLOWED.value().toString(),
+            "message" to "許可されていないメソッドです"
+    ), HttpStatus.METHOD_NOT_ALLOWED)
 
     @ResponseStatus(HttpStatus.UNAUTHORIZED)
     @ExceptionHandler(BadCredentialsException::class)
     @ResponseBody
-    fun tokenError(e: BadCredentialsException): Map<String, String?> {
-        return mapOf(
-                "statusCode" to HttpStatus.UNAUTHORIZED.value().toString(),
-                "message" to e.message
-        )
-    }
+    fun tokenError(e: BadCredentialsException): ResponseEntity<Response> = ResponseEntity(mapOf(
+            "statusCode" to HttpStatus.UNAUTHORIZED.value().toString(),
+            "message" to e.message
+    ), HttpStatus.UNAUTHORIZED)
 
 }
