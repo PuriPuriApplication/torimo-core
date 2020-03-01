c3b352998d7f239cfef16198b48a8992b81b00a9
hideyuki-aoki <aoki.ba4@gmail.com>
2020-03-01 23:41:20 +0900

    modify return Response

--- src/main/kotlin/com/ppap/torimocore/application/handler/ErrorHandler.kt
+++ src/main/kotlin/com/ppap/torimocore/application/handler/ErrorHandler.kt
@@ -1,5 +1,6 @@
 package com.ppap.torimocore.application.handler
 
+import com.ppap.torimocore.constants.*
 import org.springframework.http.HttpStatus
 import org.springframework.http.ResponseEntity
 import org.springframework.security.authentication.BadCredentialsException
@@ -9,25 +10,23 @@ import org.springframework.web.bind.annotation.ControllerAdvice
 import org.springframework.web.bind.annotation.ExceptionHandler
 import org.springframework.web.bind.annotation.ResponseStatus
 
-typealias Body = Map<String, Any?>
-
 @ControllerAdvice
 class ExceptionHandler {
 
     @ResponseStatus(HttpStatus.METHOD_NOT_ALLOWED)
     @ExceptionHandler(HttpRequestMethodNotSupportedException::class)
     @ResponseBody
-    fun handleError(): ResponseEntity<Body> = ResponseEntity(mapOf(
-            "statusCode" to HttpStatus.METHOD_NOT_ALLOWED.value().toString(),
-            "message" to "許可されていないメソッドです"
+    fun handleError(): Response = ResponseEntity(mapOf(
+            STATUS_CODE to HttpStatus.METHOD_NOT_ALLOWED.value().toString(),
+            MESSAGE to "許可されていないメソッドです"
     ), HttpStatus.METHOD_NOT_ALLOWED)
 
     @ResponseStatus(HttpStatus.UNAUTHORIZED)
     @ExceptionHandler(BadCredentialsException::class)
     @ResponseBody
-    fun tokenError(e: BadCredentialsException): ResponseEntity<Body> = ResponseEntity(mapOf(
-            "statusCode" to HttpStatus.UNAUTHORIZED.value().toString(),
-            "message" to e.message
+    fun tokenError(e: BadCredentialsException): Response = ResponseEntity(mapOf(
+            STATUS_CODE to HttpStatus.UNAUTHORIZED.value().toString(),
+            MESSAGE to e.message
     ), HttpStatus.UNAUTHORIZED)
 
 }
--- src/main/kotlin/com/ppap/torimocore/presentation/controller/ControllerBase.kt
+++ src/main/kotlin/com/ppap/torimocore/presentation/controller/ControllerBase.kt
@@ -1,5 +1,6 @@
 package com.ppap.torimocore.presentation.controller
 
+import com.ppap.torimocore.constants.*
 import org.springframework.http.HttpStatus
 import org.springframework.http.ResponseEntity
 import org.springframework.web.bind.annotation.CrossOrigin
@@ -9,20 +10,15 @@ import org.springframework.web.bind.annotation.RestController
 import org.springframework.web.client.HttpClientErrorException
 import java.lang.RuntimeException
 
-typealias Body = Map<String, Any?>
-
 @CrossOrigin
 @RestController
 class ControllerBase {
 
-    private val STATUS_CODE = "statusCode"
-    private val MESSAGE = "message"
-
-    fun createResponse(status: HttpStatus): ResponseEntity<Body> =
+    fun createResponse(status: HttpStatus): Response =
             ResponseEntity(mapOf(STATUS_CODE to status.value(), MESSAGE to status.reasonPhrase), status)
 
     @ExceptionHandler(HttpClientErrorException::class)
-    fun httpClientError(e: HttpClientErrorException): ResponseEntity<Body> = when (e.statusCode) {
+    fun httpClientError(e: HttpClientErrorException): Response = when (e.statusCode) {
         // 随時必要なStatusを実装してください
         HttpStatus.BAD_REQUEST -> badRequest(e)
         HttpStatus.NOT_FOUND -> notFound(e)
@@ -30,12 +26,12 @@ class ControllerBase {
     }
 
     @ResponseStatus(HttpStatus.BAD_REQUEST)
-    private fun badRequest(e: HttpClientErrorException): ResponseEntity<Body> = createResponse(e)
+    private fun badRequest(e: HttpClientErrorException): Response = createResponse(e)
 
     @ResponseStatus(HttpStatus.NOT_FOUND)
-    private fun notFound(e: HttpClientErrorException): ResponseEntity<Body> = createResponse(e)
+    private fun notFound(e: HttpClientErrorException): Response = createResponse(e)
 
-    private fun createResponse(e: HttpClientErrorException): ResponseEntity<Body> =
+    private fun createResponse(e: HttpClientErrorException): Response =
             ResponseEntity(mapOf(STATUS_CODE to e.statusCode.value(), MESSAGE to e.message), e.statusCode)
 
 }
