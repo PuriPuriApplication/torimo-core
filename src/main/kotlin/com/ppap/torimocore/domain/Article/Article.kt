ef862e6d96cfc0663b0e4ec941c07a533d5220f7
misaosyushi <misaki.tsujita@rakus-partners.co.jp>
2020-03-08 14:53:22 +0900

    不要なコメント削除

--- src/main/kotlin/com/ppap/torimocore/domain/Article/Article.kt
+++ src/main/kotlin/com/ppap/torimocore/domain/Article/Article.kt
@@ -30,15 +30,6 @@ data class Article(
         @ManyToOne
         @JoinColumn(name = "shop_id")
         val shop: Shop
-
-//        @OneToMany
-//        @JoinColumn(name = "article_id", referencedColumnName = "id")
-//        val articleLikes: List<ArticleLike>,
-//
-//        @OneToMany
-//        @JoinColumn(name = "from_user", referencedColumnName = "user_id")
-//        val followUsers: List<FollowUser>
-
 ) : Serializable {
     @OneToMany
     @JoinTable
