18bc4918229525a535e234c270952475e092f1aa
misaosyushi <misaki.tsujita@rakus-partners.co.jp>
2020-03-08 10:43:00 +0900

    wip:いいね一覧を取得

--- src/main/kotlin/com/ppap/torimocore/presentation/dto/ArticleDto.kt
+++ src/main/kotlin/com/ppap/torimocore/presentation/dto/ArticleDto.kt
@@ -11,13 +11,13 @@ data class ArticleDto(
         val user: UserDto,
         val shop: ShopDto,
         val categories: List<CategoryDto>,
-        val isLike: Boolean
+        val articleLikes: List<ArticleLikeDto>
 )
 
 fun Article.toDto(): ArticleDto {
     val user: UserDto = this.user.toDto()
     val shop: ShopDto = this.shop.toDto()
     val categories: List<CategoryDto> = this.categories.map { it.toDto() }
-    val isLike: Boolean = this.articleLikes.isNotEmpty()
-    return ArticleDto(this.id, this.title, this.body, this.createAt, user, shop, categories, isLike)
+    val articleLikes: List<ArticleLikeDto> = this.articleLikes.map { it.toDto() }
+    return ArticleDto(this.id, this.title, this.body, this.createAt, user, shop, categories, articleLikes)
 }
